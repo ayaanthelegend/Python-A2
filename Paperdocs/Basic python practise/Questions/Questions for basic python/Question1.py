1fc3a1fb3e8effab564b11eb971232fd4955717f
bshafqat <bilalshafqat@gmail.com>
2026-08-31 23:45:52 +0500

    Implement string reversal using while loop
    Added logic to reverse a string using a while loop.

--- Paperdocs/Basic python practise/Questions/Questions for basic python/Question1.py
+++ Paperdocs/Basic python practise/Questions/Questions for basic python/Question1.py
@@ -9,8 +9,6 @@ length = len(user) # Length of input string
 uppercase = user.upper() 
 lowercase = user.lower()
 
-stringreverse = user[::-1]
-
 print(length)
 print("Uppercase:", uppercase)
 print("Lowercase:", lowercase)
@@ -19,6 +17,13 @@ print("Lowercase:", lowercase)
 #print("Uppercase:", user.upper())
 #print("Lowercase:", user.lower())
 
-
+stringreverse = user[::-1] #this is not the correct way to do it in CS P4. They want to test the logic of string operation 
 print("String Reversed:", stringreverse)
 
+#Using while loop to reach at the end and then traverse back char by char. String is an array anyway hence can use user[i]
+reversed_text = ""
+i = len(user) - 1
+while i >= 0:
+    reversed_text = reversed_text + user[i]
+    i = i - 1
+print("String Reversed by Bilal:", reversed_text)

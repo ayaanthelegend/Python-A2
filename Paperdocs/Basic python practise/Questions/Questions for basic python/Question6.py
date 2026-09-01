c3962a71c464454b7e77686c569c85d64470a1ce
bshafqat <bilalshafqat@gmail.com>
2026-09-01 09:00:57 +0500

    Add description comment for user input functionality
    Added a comment to describe the functionality of the code.

--- Paperdocs/Basic python practise/Questions/Questions for basic python/Question6.py
+++ Paperdocs/Basic python practise/Questions/Questions for basic python/Question6.py
@@ -1,3 +1,5 @@
+### Ask the user to enter a word. Output the first character, the last character, and the total number of characters in the word.
+
 word = input("Enter a word: ")
 
 lenword = len(word) # length of the word

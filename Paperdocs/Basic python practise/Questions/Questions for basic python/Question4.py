3a09173e4ae8a5a577dc9309dd7c27efed440b82
bshafqat <bilalshafqat@gmail.com>
2026-09-01 08:53:20 +0500

    Enhance validation for vehicle plate number format
    Added validation checks for vehicle plate number format.

--- Paperdocs/Basic python practise/Questions/Questions for basic python/Question4.py
+++ Paperdocs/Basic python practise/Questions/Questions for basic python/Question4.py
@@ -1,3 +1,6 @@
+#A string PlateNumber stores a vehicle registration in the format "ABC-1234" (3 letters, hyphen, 4 digits). 
+#Write code that validates whether a given string matches this exact format, checking character by character (do not use regular expressions).
+
 Platenumber = input("Enter your vehicle plate number: ")
 
 valid = True
@@ -6,7 +9,8 @@ if len(Platenumber) != 8:
     valid = False
 else:
     for i in range(0,3):
-        if Platenumber[i] != Platenumber[i].upper():
+        if Platenumber[i] != Platenumber[i].upper(): #you are only checking upper. what abt if it is a number - BUG
+        #if not Platenumber[i].isalpha() or not Platenumber[i].isupper():
             valid = False
 
     if valid == True and Platenumber[3] != "-":
@@ -15,6 +19,7 @@ else:
     if valid:
         for i in range(4,8):
             if Platenumber[i] not in "0123456789":
+            #if not Platenumber[i].isdigit():
                 valid = False
 
 if valid:
